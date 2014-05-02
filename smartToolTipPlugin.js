07f34f26158d650fe9385c097d1c64b71d4c0b31
Nobel Huang <nobel1984@gmail.com>
2014-05-02 18:15:57 +0800

    Rewrite the whole plugin to make it more usable and support Tree panel.

--- smartToolTipPlugin.js
+++ smartToolTipPlugin.js
@@ -1,46 +1,74 @@
 /**
  * Created by joshuamcdonald69124 on 1/7/14.
+ *
+ * Rewritten by nobelhuang on 4/11/2014.
+ * Tested under EXTJS 4.2.2
  */
-Ext.define('plugin.showConditionalToolTip', {
-    extend : 'Ext.AbstractPlugin',
-    alias : 'plugin.showConditionalToolTip',
-    init: function(grid){
-        grid.on('columnresize', function(){
-            grid.getView().refresh();
-        });
-        grid.on('render', function(){
-            var tm = new Ext.util.TextMetrics();
-            Ext.Array.each(grid.columns, function(column) {
-                if (column.hasCustomRenderer == true){
-                    // This column already has a renderer applied to it
-                    // so we will be adding only the tooltip after the
-                    // custom renderer has formatted the data.
-                    column.renderer = Ext.Function.createSequence(column.renderer,function(a,b,c,d,e,f,g){
-                        // There could be instances where the column actually has no value, such as
-                        // row expander, etc.. check for that and only apply the tooltip if the column
-                        // has data.
-                        if(a){
+Ext.define("plugin.showConditionalToolTip", {
+	extend: "Ext.AbstractPlugin",
+	alias: "plugin.showConditionalToolTip",
 
-                            // Check to see if the entire data string is visible in the cell, if it is then disregard
-                            // otherwise add the tooltip
-                            if ((g.ownerCt.columns[e].getEl().getWidth() || 10) <= (((tm.getSize(a).width + 15) || 0))){
-                                b.tdAttr += 'data-qtip="' + a + '"';
-                            }
-                        }
-                    });
+	init: function(grid) {
+		var that = this;
 
-                } else {
-                    // Here we do the same as above, just w/o the sequence as there is no existing renderer
-                    column.renderer = function (a,b, c, d, e, f, g){
-                        if(a){
-                            if ((g.ownerCt.columns[e].getEl().getWidth() || 10) <= (((tm.getSize(a).width + 15) || 0))){
-                                b.tdAttr += 'data-qtip="' + a + '"';
-                            }
-                            return a;
-                        }
-                    }
-                }
-            });
-        });
-    }
+		grid.on('columnresize', function(){
+			grid.getView().refresh();
+		});
+		grid.down("tableview").on('render', function(){
+			Ext.Array.each(grid.columns, function(column) {
+				if (column.xtype === 'actioncolumn') {
+					return;
+				}
+				if (column.hasCustomRenderer === true){
+					column.renderer = Ext.Function.createSequence(column.renderer,function(a,b,c,d,e,f,g){
+						b.tdAttr += 'onmouseenter="plugin.showConditionalToolTip.__tooltip(event, this)"';
+						return a;
+					});
+				} else {
+					column.renderer = function (a,b, c, d, e, f, g){
+						b.tdAttr += 'onmouseenter="plugin.showConditionalToolTip.__tooltip(event, this)"';
+						return a;
+					}
+				}
+			});
+		});
+	},
+
+	statics: {
+		__tooltip: function (evt, dom) {
+			var el = Ext.get(dom);
+			if (el.hasCls("x-grid-cell")) {
+				if (el.hasCls("x-grid-cell-treecolumn")) {
+					var cEl = el.down(".x-grid-cell-inner");
+					var tEl = el.down(".x-tree-node-text");
+					var cWidth = cEl.getWidth(true);
+					var pWidth = tEl.dom.offsetLeft - cEl.getBorderWidth("l") - cEl.getPadding("l");
+					var rWidth = tEl.getWidth(false) + pWidth;
+
+					if (rWidth + 4 >= cWidth) {
+						tEl.set({"data-qtip": tEl.dom.innerHTML});
+						Ext.QuickTips.getQuickTip().update(tEl.dom.innerHTML);
+						Ext.QuickTips.getQuickTip().show();
+					}
+					else {
+						tEl.dom.removeAttribute("data-qtip");
+					}
+				}
+				else {
+					var tEl = el.down(".x-grid-cell-inner");
+					var cWidth = tEl.getWidth(true);
+					var rWidth = tEl.getTextWidth();
+
+					if (rWidth + 4 >= cWidth) {
+						tEl.set({"data-qtip": tEl.dom.innerHTML});
+						Ext.QuickTips.getQuickTip().update(tEl.dom.innerHTML);
+						Ext.QuickTips.getQuickTip().show();
+					}
+					else {
+						tEl.dom.removeAttribute("data-qtip");
+					}
+				}
+			}
+		}
+	}
 });
